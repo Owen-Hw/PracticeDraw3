04a7282dbdaefc1fe7aa4bd6cfd384abff7d30e6
owenlSmile@163.com <owen!@#110>
2017-10-17 10:49:17 +0800

    paint detail

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -11,12 +12,8 @@ import android.view.View;
 public class Practice13GetTextBoundsView extends View {
     Paint paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
-    String text1 = "A";
-    String text2 = "a";
-    String text3 = "J";
-    String text4 = "j";
-    String text5 = "Â";
-    String text6 = "â";
+    String[] text = new String[]{"A", "a", "J", "j", "Â", "â"};
+    int[] bounds = new int[text.length];
     int top = 200;
     int bottom = 400;
 
@@ -37,6 +34,12 @@ public class Practice13GetTextBoundsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        Rect boundRect = new Rect();
+        for (int i = 0, len = text.length; i < len; i++) {
+            paint2.getTextBounds(text[i], 0, text[i].length(), boundRect);
+            bounds[i] = -(boundRect.top + boundRect.bottom) / 2;
+        }
     }
 
     @Override
@@ -50,11 +53,8 @@ public class Practice13GetTextBoundsView extends View {
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        for (int i = 0, len = text.length; i < len; i++) {
+            canvas.drawText(text[i], 100*(i+1), middle+bounds[i], paint2);
+        }
     }
 }
